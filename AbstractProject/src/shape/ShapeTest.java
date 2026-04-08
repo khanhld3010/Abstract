91c95b23aa673a34127494e58102bfeb26cf085d
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-08 13:38:06 +0700

    Comparable

---
+++ AbstractProject/src/shape/ShapeTest.java
@@ -0,0 +1,10 @@
+package shape;
+
+public class ShapeTest {
+    static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
---
+++ AbstractProject/src/shape/circle/ComparableCircleTest.java
@@ -0,0 +1,21 @@
+package shape.circle;
+
+import java.util.Arrays;
+
+public class ComparableCircleTest {
+    static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle("red", true, 3.5);
+        System.out.println("Pre-sorted: ");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+        Arrays.sort(circles);
+        System.out.println("After-sorted: ");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
